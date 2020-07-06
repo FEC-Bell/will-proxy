32939aa78b439954150c976a9663530cdc870e49
William Hudson <williamdhudson@gmail.com>
2020-07-06 15:44:10 -0400

    Initial setup for proxy

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
