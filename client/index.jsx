217ccb60d0f53eafc4d21f57ed64cd41e6e452e0
William Hudson <williamdhudson@gmail.com>
2020-07-21 22:31:51 -0400

    renders same photos and videos for specific id

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+html {
+  background: #1b2838;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+`;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle>
+          <div>
+            <div id='reviews-graph'></div>
+            <div id='description-placeholder'></div>
+            <div id="photo-carousel"></div>
+            <div id='dlc'></div>
+            <div id='tags'></div>
+            <div id='reviews'></div>
+          </div>
+        </GlobalStyle>
+      </React.Fragment>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- index.js
+++ index.js
@@ -1,11 +1,86 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const Axios = require('axios');
 
-var app = express();
-app.use(express.static(__dirname + '/../public'));
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+const PORT = 3000;
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/public')));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+
+app.get('/app/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
+
+
+app.get('/api/gamereviews/:gameId', (req, res) => {
+  Axios.get('http://ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3001/api/gamereviews/' + req.params.gameId)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+
+
+app.get('/api/reviewcount/:gameId', (req, res) => {
+
+  Axios.get('http://localhost:3002/api/reviewcount/' + req.params.gameId)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+
+app.get('/api/tags/:gameId', (req, res) => {
+  Axios.get('http://localhost:3006/api/tags/' + req.params.gameId)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+
+
+app.get('/api/dlc/:gameId', (req, res) => {
+  Axios.get('http://localhost:3003/api/dlc/' + req.params.gameId)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+
+
+app.get('/api/name/:gameId', (req, res) => {
+  let param = req.params.gameId;
+
+});
+
+
+app.get('/api/media/:gameId', (req, res) => {
+
+  // This gets the same photos when I run on local host. Need to confirm when deployed
+  Axios.get('http://ec2-18-188-192-44.us-east-2.compute.amazonaws.com:3004/api/media/' + req.params.gameId)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
